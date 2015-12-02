64db56f5289d752e9062b4065942349425c6fa4a
William Karavites <wkaravites@gmail.com>
2015-12-02 14:24:12 -0500

    FizzBuzz for Java

--- java/FizzBuzzV1.java
+++ java/FizzBuzzV1.java
@@ -1,32 +1,3 @@
-/*
- * The MIT License
- *
- * Copyright 2015 willkara.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-/**
- * A simple FizzBuzz implementation counting from 0-1000.
- *
- * @author willkara
- */
 public class FizzBuzzV1 {
 
     /**
@@ -34,7 +5,7 @@ public class FizzBuzzV1 {
      */
     public static void main(String[] args) {
         for (int i = 1; i < 1000; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+            if (i % 15 == 0) {
                 System.out.println(i + " FIZZ BUZZ!");
             } else if (i % 3 == 0) {
                 System.out.println(i + " FIZZ!");
